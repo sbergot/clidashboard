5f85cbee0612784924920da21ec3c46e05734f5e
Simon Bergot <simon.bergot@gmail.com>
2016-08-19 07:02:36 +0200

    close socket client at the end of the stream

--- broadcastserver.js
+++ broadcastserver.js
@@ -2,10 +2,10 @@ var WebSocketServer = require("websocketserver");
 var outserver = new WebSocketServer("none", 9000);
 var inserver = new WebSocketServer("none", 9001);
 inserver.on("message", function(data) {
-     var mes = inserver.unmaskMessage(data);
-     var obj = JSON.parse(inserver.convertToString(mes.message));
-     if (obj.command === "feed") {
-         var response = {message: obj.message, time: Date.now()};
-        outserver.sendMessage('all', JSON.stringify(response));
-     }
+    var mes = inserver.unmaskMessage(data);
+    var obj = JSON.parse(inserver.convertToString(mes.message));
+    if (obj.command === "feed") {
+        var response = {message: obj.message, time: Date.now()};
+    outserver.sendMessage('all', JSON.stringify(response));
+    }
 });
--- socketpipe.js
+++ socketpipe.js
@@ -2,13 +2,14 @@ var websocket = require('websocket-stream')
 var readline = require('readline');
 
 var ws = websocket('ws://localhost:9001')
-
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
 });
 
 rl.on('line', function(line){
     ws.write(JSON.stringify({message: line, command: "feed"}));
+}).on('close', function() {
+    process.exit(0);
 })
